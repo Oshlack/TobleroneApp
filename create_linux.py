6e79d23b9505eb9342f19d299f9d31ee44d2fa92
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2022-02-25 10:10:13 +1100

    Fix linux scipt and make bash

--- create_linux.py
+++
@@ -1,3 +0,0 @@
-python3 -m pip install pyinstaller && pip install -r requirements.txt &&  python3 pyinstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns --upx-dir=..\upx391w
-
-
