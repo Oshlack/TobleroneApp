9e212269cd061f3f5fe056cb204b897c5b5774b4
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2022-02-25 09:49:24 +1100

    Ensure linux build env includes dependencies

--- create_linux.py
+++ create_linux.py
@@ -1,3 +1,3 @@
-python3 -m pip install pyinstaller && pyinstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns --upx-dir=..\upx391w
+python3 -m pip install pyinstaller && pip install -r requirements.txt &&  python3 pyinstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns --upx-dir=..\upx391w
 
 
