4c82ee6a94543f8a98f90930c5222de1ee463364
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2022-02-24 22:34:27 +1100

    add test linux app script

---
+++ create_linux.py
@@ -0,0 +1,3 @@
+python3 -m pip install pyinstaller && pyinstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns --upx-dir=..\upx391w
+
+
