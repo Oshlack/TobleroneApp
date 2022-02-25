b15f77bb72f7e786aa44b9a9f67c45caebbb8126
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2022-02-25 13:28:19 +1100

    Assume UPX on path

--- create_linux.bash
+++ create_linux.bash
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
-python3 -m pip install pyinstaller && pip install -r requirements.txt &&  python3 -m PyInstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns --upx-dir=..\upx391w
+
+apt install upx-ucl 
+python3 -m pip install pyinstaller && pip install -r requirements.txt &&  python3 -m PyInstaller toblerone.py --name Toblerone --onefile --hidden-import pysam.libctabixproxies --add-data "bin:bin" --add-data "assets:assets" --icon icon.icns 
+
 
 
